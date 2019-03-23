fa02df8ce413ae4e1d23c427533861404f01520e
Gisli <gisli@busmoneygames.com>
2019-03-23 08:44:05 +0000

    Updating submodules

--- update_all_submodules_to_latest.bat
+++ update_all_submodules_to_latest.bat
@@ -1,3 +1,5 @@
 git submodule foreach --recursive git clean -dfx
 git submodule foreach --recursive git reset --hard
+git submodule foreach --recursive git fetch
 git submodule foreach --recursive './update_as_submodule.bat || :'
+pause
