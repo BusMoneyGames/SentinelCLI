68496842767d9a2440a736d12a8dfac3d2cf4bfe
Hilmar Tryggvason <hilmart@gmail.com>
2019-03-26 17:26:42 +0000

    Adds initial server

---
+++ server.py
@@ -0,0 +1,49 @@
+import subprocess
+from argparse import ArgumentParser
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+
+parser = ArgumentParser()
+parser.add_argument('--debug', '-D', action='store_true',
+                    help='Run in debug mode')
+parser.add_argument('--port', '-P', default=7000,
+                    type=int, help='The port to run on')
+
+args = parser.parse_args()
+
+app = Flask('sentinel')
+api = Api(app)
+CORS(app)
+
+commands = ['start', 'restart', 'kill', 'list']
+
+
+def error(err):
+    return {'error': err}
+
+
+@api.resource('/command/<string:command>')
+class Command(Resource):
+    def post(self, command):
+
+        if command not in commands:
+            return error(f'Invalid command: "{command}"')
+
+        print('Should run sentinel command:', command)
+
+        try:
+            # output = subprocess.check_output(['python', 'modo.py', 'rexec', command]).decode("utf-8")
+
+            output = f'Some text that should be returned\nWow this is some good text\nThis is good\nVery good\nYes\nThe command executed was: {command}'
+
+            return {'output': output}
+        except:
+            # TODO log/display exception
+            return error('Unexpected error')
+
+        return {'output': f'This output is from Python, you executed the command: {command}'}
+
+
+if __name__ == '__main__':
+    app.run(debug=args.debug, port=args.port)
