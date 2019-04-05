f01e62a0470b387b2cd7dc77b5c8a53c20d815f6
Hilmar Tryggvason <hilmart@gmail.com>
2019-04-05 21:16:22 +0000

    Adds build config fetching

--- server.py
+++ server.py
@@ -25,10 +25,10 @@ ue4Component = [
 sentinelConfig = [
     'python', './SentinelConfig/SentinelConfig.py']
 
-configFiles = {
-    'windows_default_client': './windows_default_client.json'
+configFileBasePath = './SentinelConfig/defaultConfig'
+configFileTypes = {
+    'build': 'buildconfigs'
 }
-
 
 def error(err):
     return {'error': err}
@@ -84,23 +84,22 @@ class Command(Resource):
 @api.resource('/config/<string:config>')
 class Config(Resource):
     def get(self, config):
-        if config not in configFiles:
-            return error('Config file not found')
+        configType = request.args['type']
+
+        if configType not in configFileTypes:
+            return error('Invalid config file type')
 
         try:
-            with open(configFiles[config]) as configFile:
+            with open(f'{configFileBasePath}/{configFileTypes[configType]}/{config}.json') as configFile:
                 data = json.load(configFile)
 
-                return {'config': data}
+                return {'output': data}
         except Exception as e:
             # TODO log/display exception
             print(e)
             return error('Unexpected error')
 
     def put(self, config):
-        if config not in configFiles:
-            return error('Config file not found')
-
         body = request.get_json()
 
         if body is None:
