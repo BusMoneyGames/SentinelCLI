8da6505556970f2554bd30375447db2275d48e7d
Gisli <gisli@busmoneygames.com>
2019-05-30 14:01:28 +0000

    Simplify and cleanup un-used code

--- make_exe.bat
+++
@@ -1,3 +0,0 @@
-%UserProfile%\AppData\Local\Programs\Python\Python36/Python.exe -m pipenv sync --dev
-%UserProfile%\AppData\Local\Programs\Python\Python36/Python.exe -m pipenv run python setup.py build
-pause
